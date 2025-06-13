96416678ec917d7861e94d5b80723d5dea88006a
hanshul22 <141112472+hanshul22@users.noreply.github.com>
2025-06-13 13:25:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Ensure all routes are properly handled by the SPA
-    historyApiFallback: true
-  }
-}); 
+   server: {
+    host: '0.0.0.0',
+    port: 5173, // Or any port you want
+  }
+});

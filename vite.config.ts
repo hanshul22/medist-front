52e536ec7b50a343ad0d7d4b1b24ef4911c7151c
hanshul22 <141112472+hanshul22@users.noreply.github.com>
2025-06-13 13:26:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+   server: {
+    host: '0.0.0.0',
+    port: 5173, // Or any port you want
+  }
 });
